df3a3d20949a5c1dbb625963da688bd81b865468
Jrbotelho <57829656+Jrbotelho@users.noreply.github.com>
2022-05-11 17:03:38 +0100

    Added thymeleaf files

---
+++ SD_Project/src/main/java/JoaoEJoao/SD_Project/SdProjectApplication.java
@@ -0,0 +1,13 @@
+package JoaoEJoao.SD_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdProjectApplication.class, args);
+	}
+
+}
